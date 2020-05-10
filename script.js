3bbcbb8d186128e647eec532f060007aa8f1efc0
amgillenwater <amgillenwater@gmail.com>
2020-05-10 14:40:49 -0400

    cleaning up some--having some issues with submit event listener

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 console.log("hello");
-
+//identify form
 let noteForm = document.querySelector('#note-form')
 console.log(noteForm)
-//event listener for submission
+
+//event listener for new note button
+noteForm.addEventListener("submit", function(event) {
+    console.log("submit pressed");
+    event.preventDefault();
+})
