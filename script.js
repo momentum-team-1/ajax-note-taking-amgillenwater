73f87cd3c0427b720b0e352b0609dc97c86a8a67
amgillenwater <amgillenwater@gmail.com>
2020-05-10 16:13:45 -0400

    fixed event listener--used form instead of main tag for html and it resolved it?

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-console.log("hello");
 //identify form
-let noteForm = document.querySelector('#note-form')
-console.log(noteForm)
+let noteForm = document.querySelector("#note-form");
+// console.log(noteForm);
 
 //event listener for new note button
-noteForm.addEventListener("submit", function(event) {
-    console.log("submit pressed");
-    event.preventDefault();
+noteForm.addEventListener('submit',function(event){
+    event.preventDefault() 
+    console.log("this happened")
+
+    let noteTextInput = document.querySelector('#notes-text')
+    let noteText = noteTextInput.value
+
+    noteTextInput.value = ''
+    // createNewNote(noteText)
 })
+
+
