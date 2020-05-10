a86fff17b3565096483846397294c1cb77d75977
amgillenwater <amgillenwater@gmail.com>
2020-05-10 16:33:17 -0400

    fetch function does save to the server

--- script.js
+++ script.js
@@ -3,15 +3,26 @@ let noteForm = document.querySelector("#note-form");
 // console.log(noteForm);
 
 //event listener for new note button
-noteForm.addEventListener('submit',function(event){
-    event.preventDefault() 
-    console.log("this happened")
+noteForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("this happened");
 
-    let noteTextInput = document.querySelector('#notes-text')
-    let noteText = noteTextInput.value
+  let noteTextInput = document.querySelector("#notes-text");
+  let noteText = noteTextInput.value;
+//   console.log(noteText)
+//create the new note in our database
+  noteTextInput.value = "";
+  createNewNote(noteText)
+});
 
-    noteTextInput.value = ''
-    // createNewNote(noteText)
+//FETCH 
+function createNewNote(noteText) {
+    fetch("http://localhost:3000/notes/" , {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ body: noteText, done: false, created: moment().format()})
 })
 
+.then(response => response.json)
+.then(data => console.log(data))}
 
