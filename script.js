fa4e6edbbe1288253f4924f982d40cd15a68daa1
amgillenwater <amgillenwater@gmail.com>
2020-05-10 22:53:38 -0400

    kinda working but everything comes up as unknown

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //identify form
 let noteForm = document.querySelector("#note-form");
 // console.log(noteForm);
+let noteList = document.querySelector('#notes')
 
 //event listener for new note button
 noteForm.addEventListener("submit", function (event) {
@@ -9,20 +10,50 @@ noteForm.addEventListener("submit", function (event) {
 
   let noteTextInput = document.querySelector("#notes-text");
   let noteText = noteTextInput.value;
-//   console.log(noteText)
-//create the new note in our database
+  //   console.log(noteText)
+  //create the new note in our database
   noteTextInput.value = "";
-  createNewNote(noteText)
+  createNewNote(noteText);
 });
 
-//FETCH 
+//FETCH
 function createNewNote(noteText) {
-    fetch("http://localhost:3000/notes/" , {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ body: noteText, done: false, created: moment().format()})
-})
+  fetch("http://localhost:3000/notes/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      body: noteText,
+      done: false,
+      created: moment().format(),
+    }),
+  })
+    .then((response) => response.json)
+    .then((data) => console.log(data));
+}
 
-.then(response => response.json)
-.then(data => console.log(data))}
+//RENDER FROM SERVER - USE GET REQUEST
+function renderNote() {
+  fetch("http://localhost:3000/notes/", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log(data);
+    //   //add note to DOM from server
+      let notes = document.createElement("ul")
+      for (let item of data) {
+          let listItem = document.createElement("li");
+          listItem.dataset.id = item.id
+          listItem.innerText = item.item
+       
+          let deletePage = document.createElement("span")
+          deletePage.id = "delete";
+          deletePage.classList.add("fa", "fa-trash", "mar-l-xs")
+          listItem.appendChild(deletePage);
+          notes.appendChild(listItem);
+      }
+      noteList.appendChild (notes)
+    })
+}
 
+renderNote ()
